bc547e801b61f281356c33502c10a737ad8afbb9
AlessandroDomini <dominiblanchalessandro@gmail.com>
2021-04-15 11:36:05 +0200

    Nueva funcion
    He añadido funcionalidad
    
    
    Signed-off-by: AlessandroDomini <dominiblanchalessandro@gmail.com>

--- miproyecto/src/miproyecto/Programa.java
+++ miproyecto/src/miproyecto/Programa.java
@@ -5,7 +5,10 @@ public class Programa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hola mundo");
+		System.out.println("Hola");
+		System.out.println("mundo");
+		System.out.println("que tal");
+		
 
 	}
 
